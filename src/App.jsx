57aa0a631fbf21e7525c1a5d8d359a882b447aea
yoshimai <117501051+yoshimai@users.noreply.github.com>
2022-11-13 14:37:19 +0900

    finish

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,26 @@
-import { useState } from "react";
+import { useState, useCallback, useMemo } from "react";
 import "./styles.css";
+import { ChildArea } from "./ChildArea";
+// useCallbackは関数のメモ化
+// useMemoは変数のメモ化
 
 export default function App() {
-  const [count, setCount] = useState(0);
-  const onClickCountUp = () => {
-    setCount(count + 1);
-  };
+  const [text, setText] = useState("");
+  const [open, setOpen] = useState(false);
+
+  const onChangeText = (e) => setText(e.target.value);
+
+  const onClickOpen = () => setOpen(!open);
+
+  const onClickClose = useCallback(() => setOpen(false), [setOpen]);
 
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
-      <p>{count}</p>
-      <button onClick={onClickCountUp}>カウントアップ</button>
+      <input value={text} onChange={onChangeText} />
+      <br />
+      <br />
+      <button onClick={onClickOpen}>表示</button>
+      <ChildArea open={open} onClickClose={onClickClose} />
     </div>
   );
 }
---
+++ src/ChildArea.jsx
@@ -0,0 +1,28 @@
+import { memo } from "react";
+// memoは必要のない再レンダリングをしない
+
+const style = {
+  width: "100%",
+  height: "200px",
+  backgroundColor: "khaki"
+};
+
+export const ChildArea = memo((props) => {
+  const { open, onClickClose } = props;
+
+  const data = [...Array(2000).keys()];
+  data.forEach(() => {
+    console.log("...");
+  });
+
+  return (
+    <>
+      {open ? (
+        <div style={style}>
+          <p>子コンポーネント</p>
+          <button onClick={onClickClose}>閉じる</button>
+        </div>
+      ) : null}
+    </>
+  );
+});
